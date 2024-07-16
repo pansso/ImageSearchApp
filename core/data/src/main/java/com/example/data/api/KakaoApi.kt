96b70f5f2ac2a0e1a33e3233f8b1e95584a534fb
Jungha So <65775701+pansso@users.noreply.github.com>
2024-07-16 15:31:37 +0900

    Update KakaoApi.kt

--- core/data/src/main/java/com/example/data/api/KakaoApi.kt
+++ core/data/src/main/java/com/example/data/api/KakaoApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface KakaoApi {
     @GET("v2/search/image")
     suspend fun getKakaoImages(
-        @Header("Authorization") authorization: String = "KakaoAK 5c483853b68511ba96cc58b84f4333d6",
+        @Header("Authorization") authorization: String = "KakaoAK ",
         @Query("query") query: String,
         @Query("sort") sort: String,
         @Query("page") page: Int,
